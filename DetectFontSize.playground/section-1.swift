b08ec689da97254f8a7ca37b897b689f46323317
Svyatoshenko "Megal" Misha <megal06@nightmail.ru>
2017-04-25 19:42:57 +0300

    updated for swift 3

--- DetectFontSize.playground/section-1.swift
+++ DetectFontSize.playground/section-1.swift
@@ -5,19 +5,34 @@ import UIKit
 
 var str = "Hello, playground"
 
-UIFont.familyNames()
-UIFont.fontNamesForFamilyName("Helvetica Neue")
-UIScreen.mainScreen().scale
+UIFont.fontNames(forFamilyName: "Helvetica Neue")
+UIScreen.main.scale
 
 let fonts = [ "HelveticaNeue-Bold", "HelveticaNeue" ]
 
-for fontname in fonts {
-	for i in  11..<72 {
-		let fontsize: CGFloat = CGFloat(i);
-		if let font = UIFont(name: fontname, size: fontsize) {
-			println( "Font \(fontname)(\(i))  xHeight(2x) = \(2*font.xHeight);\t capHeight(2x) = \(2*font.capHeight)" );
-		}
-	}
+extension UIFont {
 
+	var formattedSpecs: String {
+		return "Font \(fontName)(\(pointSize))  xHeight(2x) = \(2 * xHeight);\t capHeight(2x) = \(2 * capHeight)"
+	}
 }
 
+for fontname in fonts {
+	for i in  11..<72 {
+		let fontsize: CGFloat = CGFloat(i)
+		if let font = UIFont(name: fontname, size: fontsize) {
+			print( font.formattedSpecs )
+		}
+	}
+}
+
+(11..<72).map
+	{ fontsize in
+		return CGFloat(fontsize)
+	}
+	.map { fontsize in
+		return UIFont.systemFont(ofSize: fontsize)
+	}
+	.forEach { font in
+		print(font.formattedSpecs)
+	}
